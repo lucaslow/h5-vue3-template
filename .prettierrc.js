b80fe54429b8bcb0d568fa87b147f3ada2d357b2
lucaslow <974860016@qq.com>
2022-07-17 16:33:22 +0800

    init: 初始化新增eslint校验

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 140, // 一行的字符数，如果超过会进行换行，默认为80
+	tabWidth: 2, // 一个tab代表几个空格数，默认为80
+	useTabs: true,
+	semi: false,
+	vueIndentScriptAndStyle: true,
+	singleQuote: true,
+	proseWrap: 'never',
+	htmlWhitespaceSensitivity: 'strict',
+	endOfLine: 'auto',
+	// 开启 eslint 支持
+	eslintIntegration: true,
+	bracketSpacing: true
+}
